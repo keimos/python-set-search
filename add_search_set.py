d378ff17cdd19a28f9671e39c0ca97bcd5460af7
keimos <keithmosleyjr@gmail.com>
2021-07-12 22:58:12 -0400

    Added input prompt Updates spacing

--- add_search_set.py
+++ add_search_set.py
@@ -7,8 +7,11 @@ number_set = {23, 90, 78, 12, 34, 67}
 
 number_set.add(50)
 
-#set values printing
+# set values printing
 print(number_set)
 
 _message = "that number is not available!"
+
+# request use for a number value for search
+search_val = int(input("Input a number:"))
 
