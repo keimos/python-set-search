d7ce50595426ad7d6c4d54d71b3938cfaeb666b2
keimos <keithmosleyjr@gmail.com>
2025-02-20 22:34:51 -0500

    updated file with test

--- test_add_search_set.py
+++ test_add_search_set.py
@@ -4,5 +4,5 @@ from add_search_set import create_number_set, add_number_to_set, number_search
 class TestNumberSet(unitest.TestCase):
 
     def setUp(self):
-        # This method is called before each test. Set up intial state.
+        # This method is called before each test.
         self.number_set = create_number_set()
